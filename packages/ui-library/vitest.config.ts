d1b5116b9c0ec5408346c4e57c2f6dd230fbf8fc
Narendra Patil <narendra_patil_g@outlook.com>
2025-01-26 15:20:05 +0530

    **Add React 18.3 dependencies and restructure test files**
    Updated dependencies to include React 18.3 and React DOM. Adjusted test file directory structure and configurations to enhance maintainability and compatibility. Improved Storybook and Vitest setup to align with the updated dependencies.
    
    Signed-off-by: Narendra Patil <narendra_patil_g@outlook.com>

--- packages/ui-library/vitest.config.ts
+++ packages/ui-library/vitest.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react({jsxRuntime: "automatic"})],
     test: {
-        exclude: ['node_modules', 'dist'],
         browser: {
             enabled: true,
             provider: 'playwright',
